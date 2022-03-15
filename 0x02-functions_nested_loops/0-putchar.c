425ec1dac7b883a9d59d016fb4dc08ffefe5a32b
Danford2525 <danielmtenga28@gmail.com>
2022-03-15 12:01:33 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdlib.h>
+#include <string.h>
 /**
  *  * main - Print "Holberton" followed by a new line.
  *  * Description: You are not allowed to include standard libraries.
@@ -11,6 +11,7 @@ int main(void)
 	char holberton[] = "_putchar";
 	int i = 0;
 	while (holberton[i] != '\0')
+
 {
 	putchar(holberton[i]);
 	i++;
